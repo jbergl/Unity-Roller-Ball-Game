47b94a82e0f29ef16ab6c810e33dda89a0b28d20
jbergl <bablamos123@gmail.com>
2019-07-15 23:37:58 -0400

    First Commit
    Edded Unity Project to GitHub

---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour{
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+ 
+    void FixedUpdate ()
+    {
+        float moveHoriztonal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3 (moveHoriztonal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
